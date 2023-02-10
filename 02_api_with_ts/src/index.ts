140a2da1fecc2f6c6a2824bc4014a73a7f0772b9
irubenboy <ruben.moreno.narbona.96@gmail.com>
2023-02-10 13:31:05 +0100

    #PR02 -> parse index.js to index.ts and request

--- 02_api_with_ts/src/index.ts
+++ 02_api_with_ts/src/index.ts
@@ -1,3 +1,20 @@
-const log = require('debug')("app:*")
+import express from "express"// Import express package
+import debug from "debug"// Import debug package
+import { router } from "./routes" // Import routes
 
-log("hello world !")
+const app = express() // Create a serve with express 
+const log = debug("app:*") // Create a app debug
+
+app.use(express.json()) // Use json on request body on express
+app.use('/api', router)
+
+const PORT = process.env.PORT || 3000 // Define port
+
+const ROOT = "http://localhost" // define root route
+
+// GET to '/' route -> Show a title
+
+app.get('/', (request, response) => response.send("<h1>Welcome to Hospital Central API</h1>"))
+
+
+app.listen(PORT, () => log(`Server on ${ROOT}:${PORT}`)) // Listen port and show message

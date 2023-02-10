918b76b04eecf7a2a00924a85dc81a29245b5194
irubenboy <ruben.moreno.narbona.96@gmail.com>
2023-02-10 12:01:55 +0100

    #PR02 -> parse "common/index.js" from PR01 to "common/index.ts"

---
+++ 02_api_with_ts/src/common/index.ts
@@ -0,0 +1,12 @@
+import createHttpError from "http-errors"
+import express from "express"
+
+
+const success = (response: express.Response, status = 200, message = "ok", body = {}) => response.status(status).json({ message, body })
+
+
+const fail = (response: express.Response, error: createHttpError.HttpError | null) => {
+    const { statusCode, message } = error ? error : new createHttpError.InternalServerError()
+
+    response.status(statusCode).json({ message })
+}

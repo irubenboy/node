90269afef1f1f0a6cc5098fff89cc9ca32ad4206
irubenboy <ruben.moreno.narbona.96@gmail.com>
2023-02-20 19:07:30 +0100

    #PR03 -> Orders and order items model

--- 03_eShop/models/order.js
+++ 03_eShop/models/order.js
@@ -1,7 +1,58 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true
+    }],
+    shippingAddress1: {
+        type: String,
+        requrired: true
+    },
+    shippingAddress2: {
+        type: String,
+    },
+    city: {
+        type: String,
+        requrired: true
+    },
+    zip: {
+        type: String,
+        requrired: true
+    },
+    country: {
+        type: String,
+        requrired: true
+    },
+    phone: {
+        type: String,
+        requrired: true
+    },
+    status: {
+        type: String,
+        requrired: true,
+        default: 'Pending'
+    },
+    totalPrice: {
+        type: Number,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now
+    }
+})
 
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+})
+
+orderSchema.set('toJSON', {
+    virtuals: true,
 })
 
 exports.Order = mongoose.model('Order', orderSchema);
